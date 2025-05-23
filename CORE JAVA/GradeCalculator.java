0be0ea71878e386572d9fd45ec3444ac433de38c
FAHMEEDA TAHSEEN. S <147984953+Fahmeeda25@users.noreply.github.com>
2025-05-23 02:50:15 -0700

    Create GradeCalculator.java

---
+++ CORE JAVA/GradeCalculator.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class GradeCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter marks out of 100: ");
+        int marks = scanner.nextInt();
+        String grade;
+
+        if (marks >= 90 && marks <= 100) {
+            grade = "A";
+        } else if (marks >= 80) {
+            grade = "B";
+        } else if (marks >= 70) {
+            grade = "C";
+        } else if (marks >= 60) {
+            grade = "D";
+        } else if (marks >= 0) {
+            grade = "F";
+        } else {
+            grade = "Invalid input";
+        }
+
+        System.out.println("Your grade is: " + grade);
+
+        scanner.close();
+    }
+}
