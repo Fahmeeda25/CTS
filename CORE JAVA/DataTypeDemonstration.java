cde5bcb1ce4c86ccd134313853e60879f996b726
FAHMEEDA TAHSEEN. S <147984953+Fahmeeda25@users.noreply.github.com>
2025-05-23 02:42:26 -0700

    Create DataTypeDemonstration.java

---
+++ CORE JAVA/DataTypeDemonstration.java
@@ -0,0 +1,16 @@
+public class DataTypeDemo {
+    public static void main(String[] args) {
+        int myInt = 42;
+        float myFloat = 3.14f;
+        double myDouble = 123.456;
+        char myChar = 'A';
+        boolean myBoolean = true;
+
+     
+        System.out.println("Integer (int): " + myInt);
+        System.out.println("Float (float): " + myFloat);
+        System.out.println("Double (double): " + myDouble);
+        System.out.println("Character (char): " + myChar);
+        System.out.println("Boolean (boolean): " + myBoolean);
+    }
+}
